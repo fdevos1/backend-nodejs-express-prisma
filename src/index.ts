92b4d458b458cfaaa8ce74b1c91f4ae2b64b30fe
fdevos1 <felipe.devosm@gmail.com>
2023-10-11 19:46:07 -0300

    added origin to cors

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,11 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(router);
 app.use(errorHandler);
